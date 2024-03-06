0f3394de9ee8fa905a1202e876aab12aee7aa46f
Bachys <Bachys@mail.ru>
2024-03-06 23:56:20 +0300

    reg_forms

--- diplom/barstory/urls.py
+++ diplom/barstory/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('coctails/', views.coctails, name='coctails'),
     path('profile/', views.profile, name='profile'),
     path('login/', views.loginuser, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('logout/', views.logoutuser, name='logout'),
 ]
 
--- diplom/barstory/views.py
+++ diplom/barstory/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
@@ -26,4 +28,28 @@ def loginuser(request):
                           {'form': AuthenticationForm(), 'error': "Неверны логин или пароль"})
         else:
             login(request, user)
-            return redirect('currentuser')
+            return redirect('profile')
+
+
+def registration(request):
+    if request.method == 'GET':
+        return render(request, 'barstory/registration.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('profile')
+            except IntegrityError:
+                return render(request, 'barstory/registration.html', {'form': UserCreationForm,
+                                                                      'error': "Пользователь с таким именем уже существует"})
+        else:
+            return render(request, 'barstory/registration.html', {'form': UserCreationForm,
+                                                                  'error': "Пароли не совпадают"})
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home_page')
